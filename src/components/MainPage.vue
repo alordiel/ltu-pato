898852a05a8a8eb3814d1f3c2e41b9fee7d2c3b0
alordiel <alexander.vasilev@protonmail.com>
2023-09-28 15:12:04 +0300

    some page layout changes

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -1,27 +1,5 @@
-<script>
-import { mapState } from 'pinia'
-import ListOfExcercises from './ListOfExcercises.vue'
-import { useExcercisesStore } from '@/stores/excercises'
-
-
-export default {
-  components: {
-    ListOfExcercises
-  },
-  data() {
-    return {
-      bla:0,
-    }
-  },
-  computed: {
-    ...mapState(useExcercisesStore, ['excercises'])
-  }
-}
-</script>
-
 <template>
-  <div>
-    <h3>Изберете упражнение</h3>
-    <ListOfExcercises :excercises="excercises" />
-  </div>
+  <main>
+    What for is this page?
+  </main>
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,27 @@
+<script>
+import { mapState } from 'pinia'
+import ListOfExcercises from '../components/ListOfExcercises.vue'
+import { useExcercisesStore } from '@/stores/excercises'
 
+
+export default {
+  components: {
+    ListOfExcercises
+  },
+  data() {
+    return {
+      bla:0,
+    }
+  },
+  computed: {
+    ...mapState(useExcercisesStore, ['excercises'])
+  }
+}
+</script>
 <template>
   <main>
-    Упражнения по Патоморфология
+    <h1>Упражнения по Патоморфология</h1>
+    <h3>Изберете упражнение</h3>
+    <ListOfExcercises :excercises="excercises" />
   </main>
 </template>
