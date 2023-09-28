8d0db0301a145b4255470c1b55bda4a2589f2b8c
alordiel <alexander.vasilev@protonmail.com>
2023-09-28 17:50:12 +0300

    finished functionality

---
+++ src/components/ExcerciseExam.vue
@@ -0,0 +1,88 @@
+<script>
+export default {
+    props:['images'],
+    created() {
+        this.exam.total = this.images.length;
+    },
+    data() {
+        return {
+            started: false,
+            examImages: [],
+            exam: {
+                current: 0,
+                total: 0,
+                showTitle: false,
+            }
+        }
+    },
+    methods: {
+        initiateExam () {
+            this.started = true;
+            this.examImages = [...this.shuffle(this.images)];
+        },
+        shuffle(array) {
+            let currentIndex = array.length,  randomIndex;
+
+            // While there remain elements to shuffle.
+            while (currentIndex > 0) {
+
+                // Pick a remaining element.
+                randomIndex = Math.floor(Math.random() * currentIndex);
+                currentIndex--;
+
+                // And swap it with the current element.
+                [array[currentIndex], array[randomIndex]] = [
+                array[randomIndex], array[currentIndex]];
+            }
+
+            return array;
+        },
+        nextImage(){
+            this.exam.current ++;
+            this.exam.showTitle = false;
+        },
+        restart () {
+            this.examImages = [...this.shuffle(this.images)];
+            this.exam.current = 0
+            this.exam.showTitle = false;
+        }
+    },
+}
+</script>
+
+<template>
+    <div>
+        <div class="text-center" v-show="!started">
+            <button @click="initiateExam">START</button>
+        </div>
+        <div class="guess-image" v-if="started">
+            <figure>
+                <img :src="'/images/Practice/'+examImages[exam.current].filename" :alt="examImages[exam.current].name" width="100%">
+                <figcaption v-show="exam.showTitle">{{ examImages[exam.current].name }}</figcaption>
+            </figure>
+
+            <div class="navigation text-center">
+                <button @click="exam.showTitle = true" v-show="!exam.showTitle">Покажи наименование</button>
+                <button @click="nextImage" v-show="exam.current + 1 < exam.total">Следващо изображение</button>
+                <button @click="restart" v-show="exam.current + 1 === exam.total">Рестартирай</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+
+<style scoped>
+    .guess-image img{
+        width: 100%;
+        max-width: 720px;
+        display: block;
+        margin: 0 auto;
+        border: 1px solid #ddd;
+  padding: 10px;
+    }
+    .guess-image figure{
+        text-align: center;
+        font-size: 2rem;
+        margin-bottom: 20px;
+    }
+</style>
--- src/views/SingleExcercise.vue
+++ src/views/SingleExcercise.vue
@@ -1,30 +1,28 @@
 <script>
 import { mapState } from 'pinia'
+import ExcerciseExam from '../components/ExcerciseExam.vue';
 import { useExcercisesStore } from '@/stores/excercises'
 
 export default {
 
   components: {
+    ExcerciseExam,
   },
   created(){
     const currentID = this.$route.params.id;
-        console.log(currentID)
-        const index = this.excercises.findIndex(e => e.id === currentID);
-        this.currentExcercise = this.excercises[index];
-    
+    const index = this.excercises.findIndex(e => e.id === currentID);
+    this.currentExcercise = this.excercises[index];
+    this.mode = 'main';
   },
   data() {
     return {
         currentExcercise: {},
+        mode: 'main',
     }
   },
   computed: {
     ...mapState(useExcercisesStore, ['excercises']),
-   
   },
-  methods: {
-   
-  }
 }
 
 </script>
@@ -34,10 +32,52 @@ export default {
     <main class="single-excercise">
         <h1 class="text-center">{{ currentExcercise.title }}</h1>
         <h2 class="text-center">{{ currentExcercise.subtitle }}</h2>
+        <div v-show="mode === 'main'">
+            <div class="excercise-options">
+                <p class="text-center" style="width:100%">Изберете режим</p>
+                <button @click="mode = 'learning'">Обучение</button>
+                <button @click="mode = 'test'">Тест</button>
+            </div>
+        </div>
+        <div v-show="mode==='learning'">
+            <div class="back-to-option">
+                <button @click="mode = 'main'">Назад</button>
+            </div>
+            <div class="learning-mode">
+                <div class="image" v-for="(image, key) in currentExcercise.images" :key="'img-' + key">
+                    <figure>
+                        <img :src="'/images/thumbnails/'+image.filename" :alt="image.name" width="300">
+                        <figcaption>{{ image.name }}</figcaption>
+                    </figure>
+                </div>
+            </div>
+        </div>
+        <div v-if="mode === 'test'">
+            <ExcerciseExam :images="currentExcercise.images"></ExcerciseExam>
+        </div>
     </main>
 </template>
 
 <style>
+.excercise-options {
+    max-width: 300px;
+    margin: 20px auto;
+    border: 1px #fff solid;
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
+}
+.excercise-options button {
+    width: 90px;
+    text-align: center;
+    font-weight: bold;
+    border: 1px #eee solid;
+    border-radius: 5px;
+    padding: 5px 10px;
+    margin: 15px 10px;
+    cursor: pointer;
+}
+
 @media (min-width: 1024px) {
   .single-excercise {
     min-height: calc(100vh - 125px);
