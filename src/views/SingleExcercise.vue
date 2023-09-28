99356c1cda9b80574e119a957c87c64b0fed7e13
alordiel <alexander.vasilev@protonmail.com>
2023-09-28 15:52:06 +0300

    loaded the excercises data into the single template

--- src/views/SingleExcercise.vue
+++ src/views/SingleExcercise.vue
@@ -1,5 +1,46 @@
+<script>
+import { mapState } from 'pinia'
+import { useExcercisesStore } from '@/stores/excercises'
+
+export default {
+
+  components: {
+  },
+  created(){
+    const currentID = this.$route.params.id;
+        console.log(currentID)
+        const index = this.excercises.findIndex(e => e.id === currentID);
+        this.currentExcercise = this.excercises[index];
+    
+  },
+  data() {
+    return {
+        currentExcercise: {},
+    }
+  },
+  computed: {
+    ...mapState(useExcercisesStore, ['excercises']),
+   
+  },
+  methods: {
+   
+  }
+}
+
+</script>
+
+
 <template>
-    <main>
-        {{ $route.params.id }}
+    <main class="single-excercise">
+        <h1 class="text-center">{{ currentExcercise.title }}</h1>
+        <h2 class="text-center">{{ currentExcercise.subtitle }}</h2>
     </main>
 </template>
+
+<style>
+@media (min-width: 1024px) {
+  .single-excercise {
+    min-height: calc(100vh - 125px);
+  }
+}
+</style>
